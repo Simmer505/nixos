be941d0806959fcaf2c77c8bf36f7b9d474b4493
Ethan Simmons <eesimmons9105@gmail.com>
2024-05-29 16:42:35 -0500

    Fix gitea ssh

--- hosts/ankaa/home.nix
+++ hosts/ankaa/home.nix
@@ -17,7 +17,7 @@
     home.file.".config/waybar".source = ./dotfiles/waybar;
     home.file.".config/fish/config.fish".source = ./dotfiles/fish/config.fish;
     home.file.".config/alacritty/alacritty.toml".source = ./dotfiles/alacritty/alacritty.toml;
-    home.file.".config/ssh/config".source = ./dotfiles/ssh/config;
+    home.file.".ssh/config".source = ./dotfiles/ssh/config;
 
     # Fix for slow steam download speeds https://old.reddit.com/r/linux_gaming/comments/16e1l4h/slow_steam_downloads_try_this/
     home.file.".steam/steam/steam_dev.cfg".source = ./dotfiles/steam/steam_dev.cfg;
