8384dcda6a19c8d64a501b54bd4e005dab19526e
Ethan Simmons <eesimmons9105@gmail.com>
2024-05-29 16:14:54 -0500

    Move git config to dotfile

--- hosts/ankaa/home.nix
+++ hosts/ankaa/home.nix
@@ -17,18 +17,11 @@
     home.file.".config/waybar".source = ./dotfiles/waybar;
     home.file.".config/fish/config.fish".source = ./dotfiles/fish/config.fish;
     home.file.".config/alacritty/alacritty.toml".source = ./dotfiles/alacritty/alacritty.toml;
+    home.file.".config/git/config".source = ./dotfiles/git/config;
 
     # Fix for slow steam download speeds https://old.reddit.com/r/linux_gaming/comments/16e1l4h/slow_steam_downloads_try_this/
     home.file.".steam/steam/steam_dev.cfg".source = ./dotfiles/steam/steam_dev.cfg;
 
-    programs.git = {
-        enable = true;
-        userName = "Ethan Simmons";
-        userEmail  = "eesimmons9105@gmail.com";
-        extraConfig = {
-            core.sshCommand = "ssh -i /home/eesim/.ssh/id_ed25519";
-        };
-    };
 
     programs.direnv = {
         enable = true;
